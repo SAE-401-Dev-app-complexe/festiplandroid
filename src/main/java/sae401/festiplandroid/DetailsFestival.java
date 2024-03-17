f9f06b72c1881f798d83a549242470b56d116855
enzo2900 <133020135+enzo2900@users.noreply.github.com>
2024-03-17 19:14:17 +0100

    Correction des erreurs lors du retour sur Festival

--- src/main/java/sae401/festiplandroid/DetailsFestival.java
+++ src/main/java/sae401/festiplandroid/DetailsFestival.java
@@ -12,7 +12,6 @@ import android.view.View;
 public class DetailsFestival extends AppCompatActivity  {
 
     private int idFestival;
-    private int page;
 
 
     @Override
@@ -21,16 +20,11 @@ public class DetailsFestival extends AppCompatActivity  {
         setContentView(R.layout.details_festival);
         Intent pagePrecedente = getIntent();
         idFestival = pagePrecedente.getIntExtra("idFestival",0);
-        page = pagePrecedente.getIntExtra("page",0);
-        System.out.println(page);
-
     }
 
     public void retour(View v) {
         Intent retour = new Intent(this, Festivals.class);
-        retour.putExtra("page", page);
-
-        setResult(Activity.RESULT_OK);
+        setResult(Activity.RESULT_OK,retour);
         finish();
     }
 
