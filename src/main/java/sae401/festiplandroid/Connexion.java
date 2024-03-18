7bfec74b49e77102995785ecb5f0ded2ab61d54c
Loïc Faugières <loic.faugieres@gmail.com>
2024-03-18 14:59:01 +0100

    Conventions + nouveau message erreur connexion (non renseigné)

--- src/main/java/sae401/festiplandroid/Connexion.java
+++ src/main/java/sae401/festiplandroid/Connexion.java
@@ -1,3 +1,7 @@
+/*
+ * Connexion.java                                        18 mar. 2024
+ * IUT de Rodez, pas de copyright ni de "copyleft".
+ */
 package sae401.festiplandroid;
 
 import androidx.appcompat.app.ActionBar;
@@ -9,17 +13,26 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-
 import org.json.JSONArray;
-import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
+/**
+ * Gestion de la connexion de l'utilisateur.
+ * <p>
+ *     Cette classe permet à l'utilisateur de se connecter à l'application.
+ *     Elle vérifie que les identifiants sont corrects et redirige l'utilisateur
+ *     vers la page principale de l'application.
+ *     <br>
+ *     Si les identifiants sont incorrects, un message d'erreur est affiché.
+ * </p>
+ *
+ * @author Enzo  Cluzel
+ * @author Lucas Descriaud
+ * @author Loïc  Faugières
+ * @author Simon Guiraud
+ */
 public class Connexion extends AppCompatActivity {
 
     private TextView connexionErreur;
@@ -28,6 +41,14 @@ public class Connexion extends AppCompatActivity {
 
     private EditText motDePasse;
 
+    /**
+     * Méthode appelée à la création de l'activité.
+     *
+     * @param savedInstanceState If the activity is being re-initialized after
+     *     previously being shut down then this Bundle contains the data it most
+     *     recently supplied in {@link #onSaveInstanceState}.
+     *     <b><i>Note: Otherwise it is null.</i></b>
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,6 +66,12 @@ public class Connexion extends AppCompatActivity {
         barre.setBackgroundDrawable(getResources().getDrawable(R.drawable.fond_barre_action));
     }
 
+    /**
+     * Méthode appelée lors du clic sur le bouton de connexion.
+     *
+     * @param vue La vue actuelle.
+     * @throws UnsupportedEncodingException Si l'encodage des données échoue.
+     */
     public void clicConnexion(View vue) throws UnsupportedEncodingException {
         String login = pseudo.getText().toString();
         String mdp = motDePasse.getText().toString();
@@ -57,31 +84,34 @@ public class Connexion extends AppCompatActivity {
 
         // TODO passer données dans le contenu de la requête POST
 
-        if (login == null || mdp == null || login.isEmpty() || mdp.isEmpty()) {
+        if (login.isEmpty() || mdp.isEmpty()) {
             connexionErreur.setVisibility(View.VISIBLE);
-            connexionErreur.setText(R.string.connexion_erreur_connexion);
+            connexionErreur.setText(R.string.connexion_non_renseigne);
         } else {
 
             //TODO changer url à celle correcte
-            ApiManager.appelApiGet("https://geo.api.gouv.fr/regions",this,new ListenerApi() {
+            ApiManager.appelApiGet("https://geo.api.gouv.fr/regions",
+                                   this, new ListenerApi() {
 
                 @Override
                 public void onReponsePositive(JSONArray reponseApi) {
                     // TODO modifier pour ajouter la verification de la reponse de l'api
-                    connexionReussi();
+                    gestionConnexionReussie();
                 }
 
                 @Override
                 public void onReponseErreur(String erreur) {
                     connexionErreur.setVisibility(View.VISIBLE);
-
                     connexionErreur.setText(erreur);
                 }
             });
         }
     }
 
-    private void connexionReussi() {
+    /**
+     * Si la connexion est réussie, redirige l'utilisateur vers la page principale.
+     */
+    private void gestionConnexionReussie() {
         Intent pageFestivals = new Intent(this, Festivals.class);
 
         startActivity(pageFestivals); // TODO choisir avec communication ou sans
