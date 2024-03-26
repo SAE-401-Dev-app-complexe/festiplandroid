714eed81e0a508846cf587120381141a8324b179
enzo <enzo.cluzel@iut_rodez.fr>
2024-03-26 11:22:03 +0100

    Ajout de commentaires sur les gets et attributs de la classe InfosFestival

--- src/main/java/sae401/festiplandroid/InfosFestival.java
+++ src/main/java/sae401/festiplandroid/InfosFestival.java
@@ -16,22 +16,28 @@ import java.io.Serializable;
  */
 public class InfosFestival implements Serializable {
 
+    /** Clé du festival */
     public final static String CLE_FESTIVAL = "festivalCle";
 
+    /** Id du festival */
     private int idFestival;
 
     /** Titre du festival */
     private String titre;
 
+    /** Description du festival */
     private String description;
 
     /** Lien de l'illustration ou identifiant de celle par défaut */
     private int illustration;
 
+    /** Festival favori par rapport à l'utilisateur */
     private boolean favori;
 
+    /** Date de début du festival */
     private String dateDeb;
 
+    /** Date de fin du festival */
     private String dateFin;
 
     /**
@@ -64,26 +70,48 @@ public class InfosFestival implements Serializable {
         return illustration;
     }
 
+    /**
+     * Renvoie la date de fin du festival
+     * @return une chaîne correspondant à la date de fin
+     */
     public String getDateFin() {
         return dateFin;
     }
 
+    /**
+     * Renvoie la date de début du festival
+     * @return une chaîne correspondant à la date de début
+     */
     public String getDateDeb() {
         return dateDeb;
     }
 
+    /**
+     * Renvoie l'identifiant du festival
+     * @return un entier correspondant à l'identifiant
+     */
     public int getIdFestival() {
         return idFestival;
     }
 
+    /**
+     * Renvoie la description du festival
+     * @return une chaîne correspondant à la description
+     */
     public int getDescription() {
         return idFestival;
     }
 
+    /**
+     * @return true si le festival est un festival favori pour l'utilisateur sinon false
+     */
     public boolean isFavori() {
         return favori;
     }
 
+    /**
+     * Permet de modifier l'état de favori pour un festival
+     */
     public void setFavori(boolean favori) {
         this.favori = favori;
     }
